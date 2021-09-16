cdb38a8a942d35e135758efc6941eb1a1e0535f2
Zeynab Manafova <zeynabmh1995@gmail.com>
2021-09-16 00:30:08 -0400

    added the mobile menu

---
+++ script.js
@@ -0,0 +1,21 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const burger = document.querySelector('.burger-container')
+
+// will decide later what to do with it. 
+
+// const linkHome = document.querySelector('#link-home');
+// const linkAbout = document.querySelector('#link-about');
+// const linkBlog = document.querySelector('#link-blog');
+// const linkContact = document.querySelector('#link-contact');
+
+
+burger.addEventListener('click', toggleMobileMenu)
+
+function toggleMobileMenu() {
+    if (mobileMenu.classList.contains('show-nav')) {
+        mobileMenu.classList.remove('show-nav')
+    } else {
+        mobileMenu.classList.add('show-nav')
+    }
+}
+
